ea7d59bb6819be6ee4a2808ce8937e0a979ab123
elf-dignity <anomabros@gmail.com>
2024-10-05 14:00:06 +0700

    mengedit file phpnya dari awal file yg terhack ke file php asli

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b972f07da84b618db71cf64821f10cb7b77fec71b9b299306d25caccef5dbbce
-size 419
+oid sha256:4476e73f4445cf0e7799bc257eb994862c524668a9bcaf205369b01e2046c00c
+size 420
